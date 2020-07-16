7707b037b30a0fd418373ef39fa48104ac1e697f
ykh2k <khilchenko.yehor@gmail.com>
2020-07-16 03:29:40 +0300

    fixup! feat: init

--- out/middlewares/errorHandler.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-const errorHandler = (err, req, res) => {
-    console.error(err);
-    res.status(500).json(err);
-};
-exports.default = errorHandler;
